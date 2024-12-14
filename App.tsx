8bb59d2eff4cda52b2a2afda9c1aea46ea253cc3
Fabiano Braga <fabianobragadev@gmail.com>
2024-12-14 11:02:04 -0300

    feature/

--- App.tsx
+++ App.tsx
@@ -3,16 +3,40 @@ import {Text} from './src/components/Text/Text';
 import {Button} from './src/components/Button/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+
+import {Icon} from './src/components/Icon/Icon';
 import {Box} from './src/components/Box/Box';
+import {TextInput} from './src/components/TextInput/TextInput';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 20}}>
-          <Text preset="headingLarge">Ola</Text>
-          <Button title="entrar" mb="s20" preset="outline" disabled />
-          <Button loading title="entrar" disabled />
+          <Text preset="headingLarge" mb="s8">
+            Olá!
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+
+          <TextInput
+            label="E-mail"
+            errorMessage="erro"
+            placeholder="Digite seu e-mail"
+            boxProps={{mb: 's20'}}
+          />
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            RightComponent={<Icon name="eyeOn" color="gray2" />}
+          />
+
+          <Text mt="s10" color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
+          </Text>
+          <Button mt="s48" title="Entrar" />
+          <Button mt="s12" title="Criar uma conta" preset="outline" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -4,14 +4,14 @@ import {
   ActivityIndicatorProps,
   ActivityIndicator as RNActivityIndicator,
 } from 'react-native';
-import {Theme, ThemeColors} from '../../theme/theme';
-import {useTheme} from '@shopify/restyle';
+import {ThemeColors} from '../../theme/theme';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 type Props = Omit<ActivityIndicatorProps, 'color'> & {
   color: ThemeColors;
 };
 
 export function ActivityIndicator({color, ...activityIndicatorProps}: Props) {
-  const {colors} = useTheme<Theme>();
+  const {colors} = useAppTheme();
   return <RNActivityIndicator color={colors[color]} {...ActivityIndicator} />;
 }
